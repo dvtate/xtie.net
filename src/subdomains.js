9c8e4385de74642e32a1857105d70684c1dc9917
DV Tate Testa <toast27@gmail.com>
2020-09-25 11:13:32 -0500

    comments

--- src/subdomains.js
+++ src/subdomains.js
@@ -4,15 +4,18 @@ const { cache } = require('./cache');
 // Middleware to handle subdomain redirects
 const pivot = '.' + process.env.HOSTNAME;
 module.exports = async (req, res, next) => {
-    // Get url
+    // Verify it's actually a subdomain
     const [subdomain, found] = req.hostname.split(pivot);
     if (found === undefined)
         return next();
 
     // Check redirects
     const r = cache[subdomain];
-    if (!r)
-        return res.redirect(404, process.env.HOSTNAME);
+    if (!r) {
+        res.redirect(404, process.env.HOSTNAME);
+        debug(`No rule for ${subdomain}`);
+        return;
+    }
 
     // Redirect them
     const dest = r.destination + req.path;
